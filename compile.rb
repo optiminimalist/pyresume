df12c6ada6d3ffca2fafd45ea0382566393bb94e
Michael Luckeneder <michael@luckeneder.net>
2012-11-04 18:10:43 +0000

    Nokogiri

--- compile.rb
+++ compile.rb
@@ -1,21 +1,9 @@
 #!/usr/bin/env ruby
 
-# require 'xml/xslt'
 
-# XML::XSLT.registerErrorHandler { |string| puts string }
-
-# xslt = XML::XSLT.new()
-# xslt.xml = "resume.xml"
-# xslt.xsl = "resume.xsl"
-
-# out = xslt.serve()
-
-# f= File.open("output/resume.html", "w") do |file|
-# 	file.puts out
-# end
 require 'nokogiri'
-doc   = Nokogiri::XML(File.read('resume.xml'))
 
+doc   = Nokogiri::XML(File.read('resume.xml'))
 xslt  = Nokogiri::XSLT(File.read('resume.xsl'))
 
 
